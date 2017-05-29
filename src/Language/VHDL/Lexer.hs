0f2959bf9dc749fbe9cf1959b6c26485712f79e4
Truls Asheim <truls@asheim.dk>
2017-05-29 23:36:10 +0200

    Fix string parsing (hopefully)

--- src/Language/VHDL/Lexer.hs
+++ src/Language/VHDL/Lexer.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS -fno-warn-missing-signatures #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Language.VHDL.Lexer
   ( identifier
@@ -32,6 +33,7 @@ module Language.VHDL.Lexer
   , commaSep1
   ) where
 
+import           Data.Char                 (chr)
 import           Language.VHDL.Parser.Util
 import           Language.VHDL.Syntax
 import           Text.Parsec
@@ -278,7 +280,7 @@ reserved = P.reserved lexer
 operator = P.operator lexer
 reservedOp = P.reservedOp lexer
 charLiteral = antiQ' identifier AntiClit $ CLit <$> P.charLiteral lexer
-stringLiteral = antiQ' identifier AntiSlit $ SLit <$> P.stringLiteral lexer
+stringLiteral = antiQ' identifier AntiSlit $ SLit <$> stringLiteral'
 natural = P.natural lexer
 integer = P.integer lexer
 float = P.float lexer
@@ -302,3 +304,378 @@ semiSep = P.semiSep lexer
 semiSep1 = P.semiSep1 lexer
 commaSep = P.commaSep lexer
 commaSep1 = P.commaSep1 lexer
+
+-- The following is somewhat inspired by the language module of parsec
+
+-- LRM08 15.7. Parses a string consisting of string segments and escape codes
+-- separated by &
+-- FIXME: Test escape codes
+stringLiteral' = lexeme (strSegment >>= rest) <?> "String lit"
+  where
+    rest ctx =
+      choice
+        [ symbol "&" >> strSegment >>= (\s -> rest (ctx ++ s))
+        , symbol "&" >> asciiCode >>= (\s -> rest (ctx ++ [s]))
+        , pure ctx
+        ]
+
+-- Parses a segment between " and "
+strSegment =
+  lexeme
+    (between (char '"') (char '"' <?> "end of string") (many strChar) <?>
+     "literal string")
+
+-- "" in a string becomes literal "
+strChar = try $ (string "\"\"" *> pure '"') <|> graphicalChar
+
+-- escape codes
+asciiCode = lexeme charAscii <?> "escape code"
+
+-- Parses names of unprintable ASCII chars such as ACK
+charAscii = choice (map parseAscii asciiMap)
+  where
+    parseAscii (asc, code) = try (string asc *> pure code)
+
+-- LRM08 15.2. Values as defined by the CHARACTERS type in the STANDARD package
+graphicalChar = choice (map char gchars) <?> "graphical character"
+  where
+    gchars =
+      [ ' '
+      , '!'
+        -- intentionally removed: , '"'
+      , '#'
+      , '$'
+      , '%'
+      , '&'
+      , '\''
+      , '('
+      , ')'
+      , '*'
+      , '+'
+      , ','
+      , '-'
+      , '.'
+      , '/'
+      , '0'
+      , '1'
+      , '2'
+      , '3'
+      , '4'
+      , '5'
+      , '6'
+      , '7'
+      , '8'
+      , '9'
+      , ':'
+      , ';'
+      , '<'
+      , '='
+      , '>'
+      , '?'
+      , '@'
+      , 'A'
+      , 'B'
+      , 'C'
+      , 'D'
+      , 'E'
+      , 'F'
+      , 'G'
+      , 'H'
+      , 'I'
+      , 'J'
+      , 'K'
+      , 'L'
+      , 'M'
+      , 'N'
+      , 'O'
+      , 'P'
+      , 'Q'
+      , 'R'
+      , 'S'
+      , 'T'
+      , 'U'
+      , 'V'
+      , 'W'
+      , 'X'
+      , 'Y'
+      , 'Z'
+      , '['
+      , '\''
+      , ']'
+      , '^'
+      , '_'
+      , '`'
+      , 'a'
+      , 'b'
+      , 'c'
+      , 'd'
+      , 'e'
+      , 'f'
+      , 'g'
+      , 'h'
+      , 'i'
+      , 'j'
+      , 'k'
+      , 'l'
+      , 'm'
+      , 'n'
+      , 'o'
+      , 'p'
+      , 'q'
+      , 'r'
+      , 's'
+      , 't'
+      , 'u'
+      , 'v'
+      , 'w'
+      , 'x'
+      , 'y'
+      , 'z'
+      , '{'
+      , '|'
+      , '}'
+      , '~'
+      , ' '
+      , '¡'
+      , '¢'
+      , '£'
+      , '¤'
+      , '¥'
+      , '¦'
+      , '§'
+      , '¨'
+      , '©'
+      , 'ª'
+      , '«'
+      , '¬'
+      , '\173' -- -
+      , '®'
+      , '¯'
+      , '°'
+      , '±'
+      , '²'
+      , '³'
+      , '´'
+      , 'µ'
+      , '¶'
+      , '·'
+      , '¸'
+      , '¹'
+      , 'º'
+      , '»'
+      , '¼'
+      , '½'
+      , '¾'
+      , '¿'
+      , 'À'
+      , 'Á'
+      , 'Â'
+      , 'Ã'
+      , 'Ä'
+      , 'Å'
+      , 'Æ'
+      , 'Ç'
+      , 'È'
+      , 'É'
+      , 'Ê'
+      , 'Ë'
+      , 'Ì'
+      , 'Í'
+      , 'Î'
+      , 'Ï'
+      , 'Ð'
+      , 'Ñ'
+      , 'Ò'
+      , 'Ó'
+      , 'Ô'
+      , 'Õ'
+      , 'Ö'
+      , '×'
+      , 'Ø'
+      , 'Ù'
+      , 'Ú'
+      , 'Û'
+      , 'Ü'
+      , 'Ý'
+      , 'Þ'
+      , 'ß'
+      , 'à'
+      , 'á'
+      , 'â'
+      , 'ã'
+      , 'ä'
+      , 'å'
+      , 'æ'
+      , 'ç'
+      , 'è'
+      , 'é'
+      , 'ê'
+      , 'ë'
+      , 'ì'
+      , 'í'
+      , 'î'
+      , 'ï'
+      , 'ð'
+      , 'ñ'
+      , 'ò'
+      , 'ó'
+      , 'ô'
+      , 'õ'
+      , 'ö'
+      , '÷'
+      , 'ø'
+      , 'ù'
+      , 'ú'
+      , 'û'
+      , 'ü'
+      , 'ý'
+      , 'þ'
+      , 'ÿ'
+      ]
+
+-- escape code tables
+asciiMap = zip (asciiNames ++ ascii2Names) (asciiCodes ++ ascii2Codes)
+
+asciiNames =
+  [ "NUL"
+  , "SOH"
+  , "STX"
+  , "ETX"
+  , "EOT"
+  , "ENQ"
+  , "ACK"
+  , "BEL"
+  , "BS"
+  , "HT"
+  , "LF"
+  , "VT"
+  , "FF"
+  , "CR"
+  , "SO"
+  , "SI"
+  , "DLE"
+  , "DC1"
+  , "DC2"
+  , "DC3"
+  , "DC4"
+  , "NAK"
+  , "SYN"
+  , "ETB"
+  , "CAN"
+  , "EM"
+  , "SUB"
+  , "ESC"
+  , "FSP"
+  , "GSP"
+  , "RSP"
+  , "USP"
+  , "DEL"
+  ]
+
+asciiCodes =
+  [ '\NUL'
+  , '\SOH'
+  , '\STX'
+  , '\ETX'
+  , '\EOT'
+  , '\ENQ'
+  , '\ACK'
+  , '\BEL'
+  , '\BS'
+  , '\HT'
+  , '\LF'
+  , '\VT'
+  , '\FF'
+  , '\CR'
+  , '\SO'
+  , '\SI'
+  , '\DLE'
+  , '\DC1'
+  , '\DC2'
+  , '\DC3'
+  , '\DC4'
+  , '\NAK'
+  , '\SYN'
+  , '\ETB'
+  , '\CAN'
+  , '\EM'
+  , '\SUB'
+  , '\ESC'
+  , '\FS'
+  , '\GS'
+  , '\RS'
+  , '\US'
+  , '\DEL'
+  ]
+
+ascii2Names =
+  [ "C128"
+  , "C129"
+  , "C130"
+  , "C131"
+  , "C132"
+  , "C133"
+  , "C134"
+  , "C135"
+  , "C136"
+  , "C137"
+  , "C138"
+  , "C139"
+  , "C140"
+  , "C141"
+  , "C142"
+  , "C143"
+  , "C144"
+  , "C145"
+  , "C146"
+  , "C147"
+  , "C148"
+  , "C149"
+  , "C150"
+  , "C151"
+  , "C152"
+  , "C153"
+  , "C154"
+  , "C155"
+  , "C156"
+  , "C157"
+  , "C158"
+  , "C159"
+  ]
+
+ascii2Codes =
+  map
+    chr
+    [ 128
+    , 129
+    , 130
+    , 131
+    , 132
+    , 133
+    , 134
+    , 135
+    , 136
+    , 137
+    , 138
+    , 139
+    , 140
+    , 141
+    , 142
+    , 143
+    , 144
+    , 145
+    , 146
+    , 147
+    , 148
+    , 149
+    , 150
+    , 151
+    , 152
+    , 153
+    , 154
+    , 155
+    , 156
+    , 157
+    , 158
+    , 159
+    ]
